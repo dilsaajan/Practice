c76866f546475c6539520e7edcd4d9143cfef207
dilsaajan <dilsaajanduggal@hotmail.com>
2016-01-10 15:58:42 +0000

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+package com.simplydelirious.practice;
+
+public class HelloWorld {
+	//This program will print "Hello world" in the console
+	public static void main (String args[]){//Just stuff you put at the beginning, what's being passed in 
+		System.out.println("Hello world!");//Make the system (this PC) print the line ("Hello world")
+		//Congratulations, you just made your first hello world program :)
+	}
+}
